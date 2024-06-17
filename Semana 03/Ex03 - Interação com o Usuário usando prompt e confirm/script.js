fc8362b6d954c99643d163c52a465b618901905f
josue santos <josuesantossantosvf@gmail.com>
2024-06-17 17:32:49 -0300

    add validação de nome e email

--- Semana 03/Ex03 - Interação com o Usuário usando prompt e confirm/script.js
+++ Semana 03/Ex03 - Interação com o Usuário usando prompt e confirm/script.js
@@ -3,17 +3,32 @@
     console.log("Tarefa iniciada")
 
     window.alert("Você iniciou uma tarefa")
-    const nome = window.prompt("Digite seu nome: ")
+    const nome = window.prompt("Digite seu nome: ").trim()
+
+
+    if (!nome) {
+        console.log("O nome é obrigatório")
+        window.alert("O nome é obrigatório")
+        return iniciar ()
+    }
 
     const confirmacao = window.confirm("Você quer continuar essa operação?")
     if (confirmacao == true){
-        console.log("Positivo")
+        console.log("Confirmado")
     }else {
-        console.log("Negativo")
+        console.log("Negado")
         return window.alert("Operação cancelada!")
     }
 
-    const email = window.prompt ("Digite seu email:")
+    const email = window.prompt ("Digite seu email:").trim()
+
+    if (!email) {
+        console.log("O email é obrigatório")
+        window.alert("O Email é obrigatório")
+        return iniciar ()
+    }
 
     const dados = window.confirm("nome: " + nome + " " + "email: " + " " + email + " " + "Você confirma os dados?")
+
+    const cadastro = window.alert("Dados cadastrados com sucesso: " + " " + "nome: " + nome + " " + "email: " + " " + email)
 }

00d999e0e28a736091d6ccf470dcf5c6064c24c8
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2024-06-26 00:08:28 -0300

    updates of funtions e buttons

--- Semana 05/Mini Projeto/index.js
+++ Semana 05/Mini Projeto/index.js
@@ -1,23 +1,70 @@
-/** Implemente seu código abaixo. Boa sorte! */
-
-function createList () {
-    const criarItem = document.getElementById("dados_interesse").value;
-    console.log(criarItem)
-
-    let ItemList = new Array();
-
-    if (localStorage.hasOwnProperty("ItemList")){
-        ItemList = JSON.parse(localStorage.getItem("ItemList"))
-    }
-
-    ItemList.push({item: criarItem})
-
-    localStorage.setItem("ItemList", JSON.stringify(ItemList))
-
-    document.getElementById("uniq").insertAdjacentHTML('beforeend', criarItem + '<br><hr>');
-
-    const campo = document.getElementById("dados_interesse").value
 
 
+function AddInterest() {
+    let input = document.getElementById('inputTask');
+    let task = input.value;
+    if (!task) return;
 
+
+    let storageInteresses = getLocalStorage();
+    storageInteresses.push(task);
+    localStorage.setItem('meus-interesses', JSON.stringify(storageInteresses));
+    showTasks();
+    input.value = '';
 }
+
+function getLocalStorage() {
+    let myLocalStorage = localStorage.getItem('meus-interesses')
+    return myLocalStorage ? JSON.parse(myLocalStorage) : [];
+}
+
+function showTasks (){
+            let tasks = getLocalStorage();
+            let lista = document.getElementById('interesseList');
+            console.log(lista)
+            lista.innerHTML = '';
+            tasks.forEach(function(task, index) {
+                let item = document.createElement('li');
+                item.textContent = task;
+
+
+                // buttons
+                let btnDelete = document.createElement('button');
+                btnDelete.textContent = 'Excluir';
+                btnDelete.className = 'btn';
+                btnDelete.onclick = function() {
+                    deleteInterest(index);
+                };
+                item.appendChild(btnDelete);
+
+                let btnAlter = document.createElement('button');
+                btnAlter.textContent = 'Alterar';
+                btnAlter.className = 'btn';
+                btnAlter.onclick = function() {
+                    alterInterest(index, task);
+                };
+                item.appendChild(btnAlter);
+
+                lista.appendChild(item);
+            });
+}
+
+function deleteInterest(index) {
+    let tasks = getLocalStorage()
+    tasks.splice(index, 1);
+    localStorage.setItem('meus-interesses', JSON.stringify(tasks));
+    showTasks();
+    console.log("Excluir")
+}
+
+function alterInterest(index, currentDescription) {
+    let newDescription = prompt("Alterar interesse", currentDescription)
+    if (newDescription) {
+        let task = getLocalStorage();
+        task[index] = newDescription
+        localStorage.setItem('meus-interesses', JSON.stringify(task));
+        showTasks()
+    }
+}
+
+document.addEventListener('DOMContentLoaded', showTasks);
